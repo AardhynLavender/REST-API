86dd05e99e38e3225002462b4998d5b4bb6fa745
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-03-01 17:14:53 +1300

    feat: basic Express configuration

--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log('Hello, World!')
+import express, { Request, Response } from 'express'
+import process from 'process'
+
+const PORT: string = '4000' || process.env.PORT
+const application = express()
+
+interface BasicResponse {
+	message: string
+}
+
+application.get('/', (req: Request, res: Response) => {
+	const basicResponse: BasicResponse = {
+		message: 'You have fetched the API! whoa!',
+	}
+	res.status(200).json(basicResponse)
+})
+
+application.listen(PORT, () => console.log('listening on ' + PORT + '!'))
