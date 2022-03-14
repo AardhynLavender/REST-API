a5ca8f6c2b485b5367dda9c982d41a01e48e437f
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-03-15 12:53:18 +1300

    feat: Add more fields to ingredient model

--- models/ingredient.ts
+++ models/ingredient.ts
@@ -2,10 +2,18 @@ import { Schema, Model, model } from 'mongoose'
 
 export interface IIngredient {
 	name: string
+	description?: string
+	brand?: string
+	type?: string
+	expiry?: Date
 }
 
 const ingredient: Schema<IIngredient> = new Schema<IIngredient>({
-	name: { type: String, required: true },
+	name: { type: String, required: true, unique: true, maxlength: 20 },
+	description: { type: String, required: false, maxlength: 200 },
+	brand: { type: String, required: false, maxlength: 20 },
+	type: { type: String, required: false, maxlength: 20 },
+	expiry: { type: Date, required: false },
 })
 
 export const Ingredient: Model<IIngredient> = model('Ingredient', ingredient)
