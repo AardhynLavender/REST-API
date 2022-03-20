26b8392010affc8b545374ae489915aef09c83c8
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-03-21 09:57:25 +1300

    feat: add MongoDB connection configuration

--- connection.ts
+++ connection.ts
@@ -1,3 +1,13 @@
 import mongoose from 'mongoose'
 
-export const connection = (url: string) => mongoose.connect(url)
+const options: any = {
+	useNewUrlParser: true,
+	autoIndex: true,
+	keepAlive: true,
+	connectTimeoutMS: 10000,
+	socketTimeoutMS: 45000,
+	family: 4,
+	useUnifiedTopology: true,
+}
+
+export const connection = (url: string) => mongoose.connect(url, options)
