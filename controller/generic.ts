9d9397a896725cea9177d54e6d770c587ce3154d
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-04-05 14:16:15 +1200

    feat: generic POST, PUT, DELETE controllers

--- controller/generic.ts
+++ controller/generic.ts
@@ -35,16 +35,16 @@ export const DefaultParams: IQueryParams = {
 }
 
 /**
- * Generates a routes that returns all `TObject` from the `TCollection` collection
- * @param { Request } req request of the server
- * @param { Response } res response from the server
+ * Generates a route that gets `TObject` document(s) from a `Model<TObject>` collection
+ * @param { Model<TObject> } collection collection to query
+ * @param { String } collectionName **plural** name of the collection
  * @param { Array<string> } interrogable array of properties that can be interrogated -- filtered and sorted by
- * @returns all `TObject` in the collection
+ * @returns A `TRequest` that responds `TObject` data from a `Model<TObject>` based on a request
  */
 export const CreateGenericGet =
 	<TObject>(
 		collection: Model<TObject>,
-		collectionName: string = '<unnamed>',
+		collectionName: string = 'objects',
 		interrogable: Array<string> = []
 	): TRequest =>
 	async (req: Request, res: Response): Promise<any> => {
@@ -54,6 +54,8 @@ export const CreateGenericGet =
 
 			if (id) {
 				const object: TObject | undefined = await collection.findById(id)
+
+				// return an appropriate status message
 				return res.status(200).json({
 					success: !!object,
 					[object ? 'data' : 'message']:
@@ -104,6 +106,115 @@ export const CreateGenericGet =
 			return res.status(500).json({
 				success: false,
 				message: err.message || `${collectionName} could not be fetched`,
+			})
+		}
+	}
+
+/**
+ * Generates a route that adds a `TObject` document to a `Model<TObject>` collection
+ * @param { Model<TObject> } collection collection to query
+ * @param { String } objectType name of the object type -- for responses and logging
+ * @returns A route that adds a `TObject` document to a `Model<TObject>` collection
+ */
+export const CreateGenericPost =
+	<TObject>(
+		collection: Model<TObject>,
+		objectType: string = 'object'
+	): TRequest =>
+	async (req: Request, res: Response): Promise<any> => {
+		try {
+			const object: TObject = req.body
+
+			if (object) {
+				// create ingredient and return updated collection
+				await collection.create(object)
+				const objects: Array<TObject> = await collection.find({})
+
+				return res.status(201).send({ success: true, data: objects })
+			} else
+				return res.status(201).json({
+					success: false,
+					message: `Please provide a ${objectType}`,
+				})
+		} catch (err) {
+			return res.status(409).json({
+				success: false,
+				message: err || `Failed to create ${objectType}`,
+			})
+		}
+	}
+
+/**
+ * Generates a route that updates `TObject` documents in a `Model<TObject>` collection
+ * @param { Model<TObject> } collection collection to query
+ * @param { String } objectType name of the object type -- for responses and logging
+ * @returns A route that updates `TObject` documents in a `Model<TObject>` collection
+ */
+export const CreateGenericPut =
+	<TObject>(
+		collection: Model<TObject>,
+		objectType: string = 'object'
+	): TRequest =>
+	async (req: Request, res: Response): Promise<any> => {
+		const { id } = req.params
+		const object: TObject = req.body
+
+		try {
+			if (object) {
+				const found = await collection.findByIdAndUpdate(id, object, {
+					runValidators: true,
+				})
+
+				if (found) {
+					const mutated: Array<TObject> = await collection.find({})
+					return res.status(200).json({ success: true, data: mutated })
+				} else
+					return res.status(400).json({
+						success: false,
+						message: `An id of ${id} yields no ${objectType} and cannot be mutated`,
+					})
+			} else
+				return res.status(404).json({
+					success: false,
+					message: `Request body contains no ${objectType}!`,
+				})
+		} catch (err) {
+			return res.status(404).json({
+				success: false,
+				message: err || 'Failed to update!',
+			})
+		}
+	}
+
+/**
+ * Generates a route that deletes `TObject` documents from a `Model<TObject>` collection
+ * @param { Model<TObject> } collection collection to query
+ * @param { String } objectType name of the object type -- for responses and logging
+ * @returns A route that deletes `TObject` documents from a `Model<TObject>` collection
+ */
+export const CreateGenericDelete =
+	<TObject>(
+		collection: Model<TObject>,
+		objectType: string = 'object'
+	): TRequest =>
+	async (req: Request, res: Response): Promise<any> => {
+		const { id } = req.params
+
+		try {
+			const component: TObject = await collection.findByIdAndRemove(id)
+
+			if (component) {
+				const mutated: Array<TObject> = await collection.find({})
+				return res.status(200).json({ success: true, data: mutated })
+			} else
+				return res.status(404).json({
+					success: false,
+					message: `An id of ${id} yields no ${objectType} and cannot be deleted`,
+				})
+		} catch (err) {
+			return res.status(404).json({
+				success: false,
+				message: err || `Failed to delete ${objectType}!`,
 			})
 		}
 	}
